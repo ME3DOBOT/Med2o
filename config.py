0ac4980b1b105ee65a5fa50588cbe94968940fba
mor3b2001 <102178779+mor3b2001@users.noreply.github.com>
2024-01-15 07:57:20 +0200

    Update config.py

--- config.py
+++ config.py
@@ -97,7 +97,7 @@ STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
 
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
 
-YOUTUBE_IMG_URL = "https://telegra.ph/file/74d7222ff0fdc7c24a938.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/7a6c5fb46057caeb5461e.jpg"
 
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
 
