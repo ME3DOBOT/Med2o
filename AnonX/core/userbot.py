129411b1d410c5db6f4939bce09fcc4fafc10e7b
Wa <122730917+STKR2@users.noreply.github.com>
2024-01-20 15:30:24 +0300

    Update userbot.py

--- AnonX/core/userbot.py
+++ AnonX/core/userbot.py
@@ -79,8 +79,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("JK_ZG")
-                await self.two.join_chat("PP_G3")
+                await self.two.join_chat("xl444")
+                await self.two.join_chat("xl444")
             except:
                 pass
             assistants.append(2)
@@ -110,8 +110,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("JK_ZG")
-                await self.three.join_chat("PP_G3")
+                await self.three.join_chat("xl444")
+                await self.three.join_chat("xl444")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("JK_ZG")
-                await self.four.join_chat("PP_G3")
+                await self.four.join_chat("xl444")
+                await self.four.join_chat("xl444")
             except:
                 pass
             assistants.append(4)
@@ -172,8 +172,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("JK_ZG")
-                await self.five.join_chat("PP_G3")
+                await self.five.join_chat("xl444")
+                await self.five.join_chat("xl444")
             except:
                 pass
             assistants.append(5)
